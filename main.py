dc59a97fd5445af7cfd89b41888a9c22686fa5e9
himdol <himdol8832@gmail.com>
2023-11-13 21:03:15 +0900

    last commit in today

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-import random
+# Calculate
+import collections.abc as co
 
 
-def print_hi(name):
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-def print_random2(num,num2):
-    print(random(num,num2))
 
 if __name__ == '__main__':
-    print("hi")
-    print_hi('PyCharm')
-    print("이건 프로그램 입니다.")
-    # print_random2(1,8)
-    # help(random)
-    dir(int)
+
+    test_list = ['one', 'two', 'three']
+    for i in range(test_list.__len__()):
+        print(i, test_list[i])
+
+    a = ['파이썬', '프로그램은', '빅데이터분석을', '위한', '필수적인', '도구이다.']
+    print(a[-1])    # 마지막 첫번째 값
+    print(a[-2:])   # 뒤로 두번째부터 마지막까지
+    print(a[:2])    # 0부터 2직전 까지 요소값
+    print(a[1:3])
+
+    zoo = ('python', 'elephant', 'penguin')
+    print(zoo)
+    len(zoo) # 튜플의 길이
+
+    for i in range(zoo):
+        print(i)
+
+
+
