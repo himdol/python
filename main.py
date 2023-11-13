9cfecd60edc005382b7737b25e2f1b3687e67e95
himdol <himdol8832@gmail.com>
2023-11-13 12:29:13 +0900

    python start

---
+++ main.py
@@ -0,0 +1,16 @@
+import random
+
+
+def print_hi(name):
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+def print_random2(num,num2):
+    print(random(num,num2))
+
+if __name__ == '__main__':
+    print("hi")
+    print_hi('PyCharm')
+    print("이건 프로그램 입니다.")
+    # print_random2(1,8)
+    # help(random)
+    dir(int)
